69216e95a9ddc33f71e31c38193b047c5d993dd9
Josh Calder <josh.calder@thinkmill.com.au>
2022-06-23 11:18:43 +1000

    update keystone

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,51 +1,57 @@
 //@ts-ignore
-import dotenv from 'dotenv';
-import { extendGraphqlSchema } from './mutations';
-import { config } from '@keystone-6/core';
-import { statelessSessions } from '@keystone-6/core/session';
-import { createAuth } from '@opensaas/keystone-nextjs-auth';
-import AzureB2C from '@opensaas/keystone-nextjs-auth/providers/azure-ad-b2c';
-import { stripeHook } from './lib/stripe';
-import { permissionsList } from './schemas/roleFields';
+import dotenv from "dotenv";
+import { extendGraphqlSchema } from "./mutations";
+import { config } from "@keystone-6/core";
+import { statelessSessions } from "@keystone-6/core/session";
+import { createAuth } from "@opensaas/keystone-nextjs-auth";
+import AzureB2C from "@opensaas/keystone-nextjs-auth/providers/azure-ad-b2c";
+import { stripeHook } from "./lib/stripe";
+import { permissionsList } from "./schemas/roleFields";
 
-import express from 'express';
-import url from 'url';
+import express from "express";
+import url from "url";
 
-import { lists } from './schemas';
+import { lists } from "./schemas";
 
 dotenv.config();
 
 let sessionSecret = process.env.SESSION_SECRET;
 
 if (!sessionSecret) {
-  if (process.env.NODE_ENV === 'production') {
+  if (process.env.NODE_ENV === "production") {
     throw new Error(
-      'The SESSION_SECRET environment variable must be set in production'
+      "The SESSION_SECRET environment variable must be set in production"
     );
   } else {
-    sessionSecret = '-- DEV COOKIE SECRET; CHANGE ME --';
+    sessionSecret = "-- DEV COOKIE SECRET; CHANGE ME --";
   }
 }
 
 let sessionMaxAge = 60 * 60 * 24 * 30; // 30 days
 
 const auth = createAuth({
-  listKey: 'User',
-  identityField: 'subjectId',
-  sessionData: `id name email isAdmin role { ${permissionsList.join(' ')} } memberships { id name status startDate renewalDate variation { id name subscription { id name }}}`,
+  listKey: "User",
+  identityField: "subjectId",
+  sessionData: `id name email isAdmin role { ${permissionsList.join(
+    " "
+  )} } memberships { id name status startDate renewalDate variation { id name subscription { id name }}}`,
   autoCreate: true,
-  userMap: { subjectId: 'id', email: 'email', name: 'name',},
-  accountMap: {},
-  profileMap: {preferredName: 'given_name', phone: 'extension_PhoneNumber'},
+  resolver: async ({ user, profile }: { user: any; profile: any }) => {
+    const preferredName = profile.given_name || user.name;
+    const phone = profile.extension_PhoneNumber;
+    const name = user.name as string;
+    const email = user.email as string;
+    return { email, name, preferredName, phone };
+  },
   sessionSecret,
-  keystonePath: '/admin',
+  keystonePath: "/admin",
   providers: [
     AzureB2C({
-      tenantId: process.env.B2C_TENANT_ID || 'tenant-id',
-      clientId: process.env.B2C_CLIENT_ID || 'ClientID',
-      clientSecret: process.env.B2C_CLIENT_SECRET || 'ClientSecret',
-      primaryUserFlow: process.env.B2C_FLOW || 'B2C_1_SignUp',
-      authorization: { params: { scope: "offline_access openid" }}
+      tenantId: process.env.B2C_TENANT_ID || "tenant-id",
+      clientId: process.env.B2C_CLIENT_ID || "ClientID",
+      clientSecret: process.env.B2C_CLIENT_SECRET || "ClientSecret",
+      primaryUserFlow: process.env.B2C_FLOW || "B2C_1_SignUp",
+      authorization: { params: { scope: "offline_access openid" } },
     }),
   ],
 });
@@ -53,11 +59,11 @@ const auth = createAuth({
 export default auth.withAuth(
   config({
     db: {
-      provider: 'postgresql',
+      provider: "postgresql",
       useMigrations: true,
       url:
         process.env.DATABASE_URL ||
-        'postgres://postgres:mysecretpassword@localhost:55000',
+        "postgres://postgres:mysecretpassword@localhost:55000",
     },
     ui: {
       isAccessAllowed: (context) => context.session?.data?.isAdmin,
@@ -76,24 +82,24 @@ export default auth.withAuth(
       healthCheck: true,
       extendExpressApp: (app, createContext) => {
         app.use(
-          '/api/stripe-webhook',
+          "/api/stripe-webhook",
           express.json({
             // We need the raw body to verify webhook signatures.
             // Let's compute it only when hitting the Stripe webhook endpoint.
             verify: function (req, res, buf) {
               const pathname = url.parse(req?.url!).pathname;
 
-              if (req.method === 'POST') {
+              if (req.method === "POST") {
                 (req as any).rawBody = buf.toString();
               }
             },
           })
         );
-        app.use('/api/stripe-webhook', async (req, res, next) => {
+        app.use("/api/stripe-webhook", async (req, res, next) => {
           (req as any).context = await createContext(req, res);
           next();
         });
-        app.post('/api/stripe-webhook', stripeHook);
+        app.post("/api/stripe-webhook", stripeHook);
       },
     },
   })
